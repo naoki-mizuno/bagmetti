f91fdf6e6d5d5b2f1c387e92a3ef648b9c853121
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2018-01-18 14:32:36 +0900

    Move main logic into function process_bag

--- scripts/filter.py
+++ scripts/filter.py
@@ -6,11 +6,6 @@ from rospy import Time
 import sys
 
 
-if __name__ != '__main__':
-    sys.stderr.write('bagmetti needs to be run as a script')
-    sys.exit(1)
-
-
 def usage():
     return 'filter.py <in bag> <out bag> <conf file>\n'
 
@@ -47,51 +42,57 @@ def get_topics(rules, all_topics=None):
     return topics
 
 
-if len(sys.argv) < 4:
-    sys.stderr.write(usage())
-    sys.exit(1)
-
-bag_in_fn, bag_out_fn, conf_file_fn = sys.argv[1:4]
-
-bag_in = Bag(bag_in_fn)
-bag_out = Bag(bag_out_fn, 'w')
+def process_bag(bag_in_fn, bag_out_fn, conf_file_fn):
+    bag_in = Bag(bag_in_fn)
+    bag_out = Bag(bag_out_fn, 'w')
 
 # Read in rules
-rules = []
-time_rules = []
-with open(conf_file_fn) as conf_file:
-    for line in conf_file:
-        rule = Rule.parse(line)
-        if rule is None:
-            continue
-        if rule.is_time():
-            time_rules.append(rule)
-        else:
-            rules.append(rule)
-Rule.set_begin_time(bag_in.get_start_time())
+    rules = []
+    time_rules = []
+    with open(conf_file_fn) as conf_file:
+        for line in conf_file:
+            rule = Rule.parse(line)
+            if rule is None:
+                continue
+            if rule.is_time():
+                time_rules.append(rule)
+            else:
+                rules.append(rule)
+    Rule.set_begin_time(bag_in.get_start_time())
 
 # Find start and end times so that we can speed up the reading as much as possible
-t_start, t_end = get_begin_end(time_rules, bag_in.get_start_time(), bag_in.get_end_time())
+    t_start, t_end = get_begin_end(time_rules, bag_in.get_start_time(), bag_in.get_end_time())
 # Same thing for topics
-all_topics = bag_in.get_type_and_topic_info().topics.keys()
-topics = get_topics(rules, all_topics)
+    all_topics = bag_in.get_type_and_topic_info().topics.keys()
+    topics = get_topics(rules, all_topics)
 
-for topic, msg, t in bag_in.read_messages(topics=topics, start_time=t_start, end_time=t_end):
-    # Normal rules
-    inclusion_ok = False
-    exclusion_ok = True
-    for r in rules:
-        if r.is_include() and r.is_ok_with(topic, msg, t):
-            inclusion_ok = True
-        elif r.is_exclude() and not r.is_ok_with(topic, msg, t):
-            exclusion_ok = False
-    # Time rules
-    time_ok = True
-    for r in time_rules:
-        if not r.is_ok_with(topic, msg, t):
-            time_ok = False
-    # Write to file
-    if (inclusion_ok or exclusion_ok) and time_ok:
-        bag_out.write(topic, msg, t)
+    for topic, msg, t in bag_in.read_messages(topics=topics, start_time=t_start, end_time=t_end):
+        # Normal rules
+        inclusion_ok = False
+        exclusion_ok = True
+        for r in rules:
+            if r.is_include() and r.is_ok_with(topic, msg, t):
+                inclusion_ok = True
+            elif r.is_exclude() and not r.is_ok_with(topic, msg, t):
+                exclusion_ok = False
+        # Time rules
+        time_ok = True
+        for r in time_rules:
+            if not r.is_ok_with(topic, msg, t):
+                time_ok = False
+        # Write to file
+        if (inclusion_ok or exclusion_ok) and time_ok:
+            bag_out.write(topic, msg, t)
 
-bag_out.close()
+    bag_out.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        sys.stderr.write(usage())
+        sys.exit(1)
+    process_bag(*sys.argv[1:4])
+else:
+    sys.stderr.write('bagmetti needs to be run as a script\n')
+    sys.exit(1)
+
