8ee339cf917d0ab27f881cfc2cc87241fead2e7d
Naoki Mizuno <mizuno.naoki@rm.is.tohoku.ac.jp>
2018-12-08 01:42:39 +0900

    [WIP] Add unit tests

---
+++ test/module/test_rename.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+import rostest
+
+import unittest
+
+from bagmetti.rules.filter import FilterRule
+
+
+class TestFilter(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
